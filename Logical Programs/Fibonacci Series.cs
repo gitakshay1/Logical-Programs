0f1b20c7d952c5f45ce42d1022049e772eb18b0f
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-08 20:51:23 +0530

    [Akshay]-Add Fibonacci Series

---
+++ Logical Programs/Fibonacci Series.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class Fibonacci_Series
+    {
+        public void Fibonacci ()
+        {
+            Console.WriteLine("Enter the Range of Fibonacci series :");
+            int Range=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter First term of series :");
+            int FirstTerm=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second term of series :");
+            int SecondTerm=Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<=Range; i++)
+            {
+                int ThirdTerm = FirstTerm + SecondTerm;
+                Console.WriteLine(ThirdTerm);
+                FirstTerm = SecondTerm;
+                SecondTerm = ThirdTerm;
+            }
+        }
+    }
+}
---
+++ Logical Programs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 Fibonacci Series");
+            int Ans=Convert.ToInt32(Console.ReadLine());
+            switch(Ans)
+            {
+                case 1:
+                    Fibonacci_Series fibonacci_Series=new Fibonacci_Series();
+                    fibonacci_Series.Fibonacci();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid Number");
+                    break;  
+            }
+        }
+    }
+}
